6965a292ad2f05b9597a372a26ba8cbd297c8854
OzcanDogan <20200808018@ogr.akdeniz.edu.tr>
2023-04-18 01:04:17 +0300

    Home automotion system
    Web based home automotion system with php html css and javascript

---
+++ HomePage_p.php
@@ -0,0 +1,72 @@
+<div>
+<div class="container">
+        <div class="navbar">
+            <div class="logo">
+                <a href="HomePage.php">Smart Home</a>
+            </div>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Communication</a></li>
+                <li><a href="#">Products</a></li>
+            </ul>
+
+        </div>  
+    <h1>What do you want to check ?</h1>
+    <a href="temperature_p.html" class="select">Temperature</a>
+    <a href="WIFI_p.html" class="select">Wifi</a>
+    <a href="Lights_p.html" class="select">Lights</a>
+    <a href="GarageDoor_p.html" class="select">Garage Door</a>
+    <a href="camera_p.html" class="select">Cameras</a>
+    <a href="Water-temperature_p.html" class="select">Water Temperature</a>
+
+</div>
+<style>
+    h1{text-align: center;
+    margin-right: 200px;}
+.select:link, .select:visited {
+        background-color: #080710;
+        color: white;
+        padding: 14px 25px;
+        text-align: center;
+        text-decoration: none;
+        display: block;
+        padding-top: 35px;
+        padding-bottom: 35px;
+        
+    }
+    .navbar .logo{
+float: left;
+margin-left: 40px;
+margin-top: 20px;
+}
+.navbar{
+   
+width: 100%;
+height: 80px;
+background-color: 080710;
+
+}
+.navbar a{
+    float: right;
+    color: white;
+    margin: 15px;
+
+}
+.navbar .logo{
+    text-decoration: none;
+    font-size: 30px;
+    margin-bottom: 30px;
+}
+.navbar ul li{
+    list-style: none;
+    display: inline;
+    font-size: 20px;
+    margin: 10px;
+
+}
+ 
+ 
+    .select:hover, .select:active {
+        background-color: red;
+    }
+    </style>
---
+++ main.php
@@ -0,0 +1,20 @@
+    <div class="firstPage" style="background-color: 080710;">
+        <div class="producer">
+            <H1 style="margin-left: 50%;">PRODUCER</H1>
+            <a href="producer.php"><img src="monitor-1307227__340.jpg" alt="" width="100%" height="50%"></a>
+        </div>
+        <div class="costumer">
+            <h1 style="margin-left: 50%;">CUSTOMER</h1>
+            <a href="customer.php"><img src="ev-otomasyon-resim2.jpg" alt=""width="100%" height="50%">></a>
+        </div>
+    </div>
+    <style>
+        h1{
+            color: white;
+        }
+        body{
+            background-color: #080710;
+        }
+    </style>
+    
+       
